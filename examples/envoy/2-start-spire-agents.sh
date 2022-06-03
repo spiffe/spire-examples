e281f31bfccc5e599be93f791bca9993b278d522
Marcos Yacob <marcos.yacob@hpe.com>
2022-06-03 10:55:45 -0300

    Update workflow to run envoy test
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- examples/envoy/2-start-spire-agents.sh
+++ examples/envoy/2-start-spire-agents.sh
@@ -4,24 +4,21 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bb=$(tput bold)
-nn=$(tput sgr0)
-
 # Bootstrap trust to the SPIRE server for each agent by copying over the
 # trust bundle into each agent container. Alternatively, an upstream CA could
 # be configured on the SPIRE server and each agent provided with the upstream
 # trust bundle (see UpstreamAuthority under
 # https://github.com/spiffe/spire/blob/main/doc/spire_server.md#plugin-types)
-echo "${bb}Bootstrapping trust between SPIRE agents and SPIRE server...${nn}"
+echo "Bootstrapping trust between SPIRE agents and SPIRE server..."
 docker-compose -f "${DIR}"/docker-compose.yml exec -T spire-server bin/spire-server bundle show |
 	docker-compose -f "${DIR}"/docker-compose.yml exec -T web tee conf/agent/bootstrap.crt > /dev/null
 docker-compose -f "${DIR}"/docker-compose.yml exec -T spire-server bin/spire-server bundle show |
 	docker-compose -f "${DIR}"/docker-compose.yml exec -T echo tee conf/agent/bootstrap.crt > /dev/null
 
 # Start up the web server SPIRE agent.
-echo "${bb}Starting web server SPIRE agent...${nn}"
+echo "Starting web server SPIRE agent..."
 docker-compose -f "${DIR}"/docker-compose.yml exec -d web bin/spire-agent run
 
 # Start up the echo server SPIRE agent.
-echo "${bb}Starting echo server SPIRE agent...${nn}"
+echo "Starting echo server SPIRE agent..."
 docker-compose -f "${DIR}"/docker-compose.yml exec -d echo bin/spire-agent run
--- examples/envoy/3-create-registration-entries.sh
+++ examples/envoy/3-create-registration-entries.sh
@@ -4,9 +4,6 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bb=$(tput bold)
-nn=$(tput sgr0)
-
 fingerprint() {
 	# calculate the SHA1 digest of the DER bytes of the certificate using the
 	# "coreutils" output format (`-r`) to provide uniform output from
@@ -17,14 +14,14 @@ fingerprint() {
 WEB_AGENT_FINGERPRINT=$(fingerprint "${DIR}"/docker/web/conf/agent.crt.pem)
 ECHO_AGENT_FINGERPRINT=$(fingerprint "${DIR}"/docker/echo/conf/agent.crt.pem)
 
-echo "${bb}Creating registration entry for the web server...${nn}"
-docker-compose -f "${DIR}"/docker-compose.yml exec spire-server bin/spire-server entry create \
+echo "Creating registration entry for the web server..."
+docker-compose -f "${DIR}"/docker-compose.yml exec -T spire-server bin/spire-server entry create \
 	-parentID spiffe://domain.test/spire/agent/x509pop/${WEB_AGENT_FINGERPRINT} \
 	-spiffeID spiffe://domain.test/web-server \
 	-selector unix:user:envoy
 
-echo "${bb}Creating registration entry for the echo server...${nn}"
-docker-compose -f "${DIR}"/docker-compose.yml exec spire-server bin/spire-server entry create \
+echo "Creating registration entry for the echo server..."
+docker-compose -f "${DIR}"/docker-compose.yml exec -T spire-server bin/spire-server entry create \
 	-parentID spiffe://domain.test/spire/agent/x509pop/${ECHO_AGENT_FINGERPRINT} \
 	-spiffeID spiffe://domain.test/echo-server \
 	-selector unix:user:envoy
