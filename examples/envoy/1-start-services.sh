f094d11f294470d0deb4329485f39e26c50bd364
Marcos Yacob <marcos.yacob@hpe.com>
2022-06-03 17:16:23 -0300

    Update workflow to run envoy test (#51)
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- examples/envoy/1-start-services.sh
+++ examples/envoy/1-start-services.sh
@@ -4,13 +4,10 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-bb=$(tput bold)
-nn=$(tput sgr0)
-
 # Start up the web server
-echo "${bb}Starting web server...${nn}"
+echo "Starting web server..."
 docker-compose -f "${DIR}"/docker-compose.yml exec -d web web-server -log /tmp/web-server.log
 
 # Start up the echo server
-echo "${bb}Starting echo server...${nn}"
+echo "Starting echo server..."
 docker-compose -f "${DIR}"/docker-compose.yml exec -d echo echo-server -log /tmp/echo-server.log
